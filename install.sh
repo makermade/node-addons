38901908571530fe10ac7f4c086d6c50a158298b
AZ technology <aztec@localhost.domain>
2015-10-22 15:11:11 +0800

    install.sh: remove the hardcode
    Change-Id: Ifa4c050c97049985303dfaae93a5264c70d61fe5

--- install.sh
+++ install.sh
@@ -14,6 +14,13 @@
 # to system build, you should add promise to Android.mk
 
 SCRIPTPATH=$(cd $(dirname $0); pwd -P)
+
+. $SCRIPTPATH/../load-config.sh
+
+if [[ ! -n $DEVICE ]]; then
+	echo DEVICE is not set
+	exit 128
+fi
 export PATH=$SCRIPTPATH/../out/host/linux-x86/bin:$PATH
 export TOOLCHAIN=$SCRIPTPATH/../toolchain/linux-x86/gcc-linaro-arm-linux-gnueabihf-4.8-2014.04_linux/bin
 export AR=$TOOLCHAIN/arm-linux-gnueabihf-ar
@@ -22,5 +29,5 @@ export CXX=$TOOLCHAIN/arm-linux-gnueabihf-g++
 export LINK=$TOOLCHAIN/arm-linux-gnueabihf-g++
 export npm_config_arch=arm
 export npm_config_nodedir=$SCRIPTPATH/../node/
-export npm_config_prefix=$SCRIPTPATH/../out/target/product/flame/system/
+export npm_config_prefix=$SCRIPTPATH/../out/target/product/$DEVICE/system/
 npm install -g --target_arch=arm $1
